334ab3be8ab77959f846f15ef0fc796e47ea4198
alessandra-m-silva <alessandra.m.silva876@gmail.com>
2026-09-10 09:21:40 -0300

    Adds the base code structure, sound effects, and sprites.

---
+++ jogo.js
@@ -0,0 +1,7 @@
+console.log('Flappy-Bird -- Des. Alessandra')
+
+const sprites = new Image();
+sprites.src = './sprites-and-effects/spites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
